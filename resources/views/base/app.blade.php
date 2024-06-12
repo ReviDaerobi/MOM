e75e209f3d367681c03a29c85fdbaa347353cfe6
ReviDaerobi <122437402+ReviDaerobi@users.noreply.github.com>
2024-06-12 17:44:48 +0700

    work day 1
    dashboard + sidebar

--- resources/views/base/app.blade.php
+++ resources/views/base/app.blade.php
@@ -8,10 +8,28 @@
     @vite('resources/js/app.js')
 </head>
 
-<body class="bg-gray-100 font-mono">
-    @yield('sidebar')
+<body class="bg-gray-900 font-mono">
+
+  <div class=" mx-auto flex">
+    <header style="background-color: #fff">
+      @yield('header')
+    </header>
+
+    <section >
+      @yield('sidebar')
+    </section>
     
-    @yield('konten')
+    <main style="">
+      <div class="mt-4">
+        <div class="bg-white rounded-lg shadow-md ml-10 p-4">
+          @yield('konten')
+
+        </div>
+
+      </div>
+    </main>
+    
+  </div>
 
     @stack('scripts')
 </body>
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,23 +1,26 @@
 @extends("base.app")
-
+@push('styles')
+<link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css" />
+<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+@endpush
 @section('sidebar')
-
+<div class="flex">
 <div class="bg-gray-800 w-64 p-4">
     <div class="text-center mb-4">
         <img src="logo.png" alt="Logo" class="w-24 h-24 rounded-full">
         <h1 class="text-white text-xl mt-2">MNC-Client</h1>
     </div>
-    <ul class="list-none p-0">
+    <ul class="list-none p-0" >
         <li class="mb-2">
             <a href="#" class="block px-4 py-2 text-gray-400 hover:bg-gray-700 hover:text-white rounded-md">
                 <i class="fas fa-home mr-2"></i> Rate Card
             </a>
         </li>
-        <li class="mb-2">
+        <li class="mb-2" id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover">
             <a href="#" class="block px-4 py-2 text-gray-400 hover:bg-gray-700 hover:text-white rounded-md">
                 <i class="fas fa-users mr-2"></i> Master Data
             </a>
-            <ul class="ml-8 list-none p-0">
+            <ul class="hidden ml-8 list-none p-0 bg-gray-800" id="dropdownHover">
                 <li class="mb-2">
                     <a href="#" class="block px-4 py-2 text-gray-400 hover:bg-gray-700 hover:text-white rounded-md">
                         <i class="fas fa-user-circle mr-2"></i> List User
@@ -77,16 +80,32 @@
         </li>
     </ul>
 </div>
+</div>
 
 @endsection
 
 @section('konten')
 
-@php
-    foreach ($datas as $data) {
-        echo $data->nama;
-    }
-@endphp
+<table id="example" class=" display cell-border" style="width:100%">
+    <thead class="" style="width: 100%">
+        <tr>
+            <th>Name</th>
+            <th>Alamat</th>
+            <th>Telepon</th>
+        </tr>
+        
+    </thead>
+    <tbody>
+    @foreach ($datas as $data)
+        <tr>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->alamat }}</td>
+            <td>{{ $data->telepon }}</td>
+        </tr>
+      
+    @endforeach
+    </tbody>
+</table>
 
 @endsection
 
@@ -96,4 +115,17 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script src="https://kit.fontawesome.com/ad1585c3d7.js" crossorigin="anonymous"></script>
 <script src="/src/js/app.js"></script>
+<script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+
+<script>
+    $('#example').DataTable( { 
+        columnDefs: [
+            {
+                targets: [0,1,2],
+                className: 'dt-head-center dt-body-center'
+            }
+        ]
+    });
+</script>
 @endpush
